6a41887683bfc32a9b3187b7a0708a245d6a8481
mppetrov <mppetrov@beeline.ru>
2023-01-28 14:20:02 +0300

    HW_2

--- src/main/scala/backoffice/api/BackOfficeAPI.scala
+++ src/main/scala/backoffice/api/BackOfficeAPI.scala
@@ -11,8 +11,8 @@ object BackOfficeAPI {
     case Method.GET -> !! / "health" =>
         ZIO.succeed(Response.json("""{"status": "OK"}"""))
 
-    case Method.GET -> !! =>
-      ZIO.succeed(Response.text(s"Hello word from ${sys.env("HOSTNAME")}"))
+    case Method.GET -> !! / "hello" / user / _ =>
+      ZIO.succeed(Response.text(s"Hello ${user} from ${sys.env("HOSTNAME")}"))
 
     case Method.GET -> !! / "version" =>
       ZIO.succeed(Response.json("""{"version": "0.0.3"}"""))
